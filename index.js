b612d1dcdf10966c3046528b526e69a6fe4a77c9
Artem71 <fich71@mail.ru>
2019-10-31 23:13:25 +0300

    module

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const weatherRequest = require('./requests/weather.request')
 
 const app = express()
 
@@ -14,9 +15,11 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
   const { city } = req.body
 
-  console.log(city)
+  weatherRequest(city)
   res.render('index')
 })
+
+// 1239d38c25b99afa42c6d427af813b77
 
 app.listen(3000, () => {
   console.log('Server has started on port 3000')
